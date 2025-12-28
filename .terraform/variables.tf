5fc30e5a67297c4a47204353b81c4de6f1a08400
craspoo <camilarspoo@gmail.com>
2025-12-28 09:47:37 -0300

    primeiro

---
+++ .terraform/variables.tf
@@ -0,0 +1,42 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "cluster_name" { type = string, default = "SistemaPedidos-eks" }
+variable "expiry_hours" { type = number, default = 24 }
+
+locals {
+  expiry_iso = timestamp(timeadd(timestamp(), "${var.expiry_hours}h"))
+}
+
+variable "tags_prod" {
+  type = map(any)
+  default = {
+    Name        = "TF-EKS",
+    Environment = "Production",
+    School = "FIAP",
+    Year = "2025"
+  }
+}
+variable "tags" {
+  default = {
+    Name = "terraform-infra-EKS"
+  }
+}
+variable "instance-type" {
+  default = "t3.medium"
+}
+
+variable "cidr_vpc" {
+  default = "10.0.0.0/16"
+}
+
+variable "principal_user_arn" {
+  type    = string
+  default = "arn:aws:iam::579375260812:user/camila"
+}
+
+variable "bucket_name_backend"
+{
+  default = "sistemapedidos-eks-backend"
+}
