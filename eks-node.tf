304917890abc1234d2e8cdf8e5d68077bdf00998
craspoo <camilarspoo@gmail.com>
2025-12-28 15:07:00 -0300

    primeiro

--- eks-node.tf
+++ eks-node.tf
@@ -25,42 +25,43 @@
 #   ]
 # }
 #
-# resource "aws_eks_node_group" "clientes" {
-#   cluster_name    = aws_eks_cluster.cluster.name
-#   node_group_name = "nodeg-clientes-${var.bucket_name_backend}"
-#   node_role_arn   = aws_iam_role.node.arn
-#   subnet_ids      = aws_subnet.subnet_public[*].id
-#   disk_size       = 50
-#   instance_types  = [var.instance-type]
-#
-#   labels = {
-#     microservice = "clientes"
-# }
-#
-# taint {
-#     key    = "microservice"
-#     value  = "clientes"
-#     effect = "NO_SCHEDULE"
-# }
-#
-# scaling_config {
-#     desired_size = 2
-#     max_size     = 3
-#     min_size     = 2
-# }
-#
-# update_config {
-#     max_unavailable = 1
-# }
-#
-#   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-#   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-# depends_on = [
-#     aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
-#     aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
-#   ]
-# }
+
+resource "aws_eks_node_group" "clientes" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "nodeg-clientes-${var.bucket_name_backend}"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = aws_subnet.subnet_public[*].id
+  disk_size       = 50
+  instance_types  = [var.instance-type]
+
+  labels = {
+    microservice = "clientes"
+}
+
+taint {
+    key    = "microservice"
+    value  = "clientes"
+    effect = "NO_SCHEDULE"
+}
+
+scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
+}
+
+update_config {
+    max_unavailable = 1
+}
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
 
 resource "aws_eks_node_group" "produtos" {
   cluster_name    = aws_eks_cluster.cluster.name
